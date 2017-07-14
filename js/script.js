f619e4e6ef3b3e29787837294d7c8c27b865b1ca
sarareich <reichsara96@gmail.com>
2017-07-14 11:13:51 -0400

    Lightbox

---
+++ js/script.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+	var img_src;
+	var slide;
+	var caption;
+
+	$(".lightbox_trigger").on("click", function() {
+		slide = $(this).attr("id");
+		openLightbox();
+	});
+
+	$(".close").on("click", function() {
+		closeLightbox();
+	});
+
+	$(".next").on("click", function() {
+		slide++;
+
+		if($("#" + slide).attr("src")) {
+			newSlide();
+		} else{
+			slide = 1;
+			newSlide();
+		}
+	});
+
+	$(".prev").on("click", function() {
+		if (slide > 1) {
+			slide--;
+		} else if (slide == 1) {
+			slide = $(".last").attr("id");
+		}
+
+		newSlide();
+	});
+
+	function openLightbox() {
+		img_src = $("#" + slide).attr("src");
+		caption = $("#" + slide).attr("alt");
+
+		if ($("#content").length > 0) {
+			$("#content").html(
+				'<img src="' + img_src + '" id="slides"/>' +
+				'<div id="lightbox_caption">' + caption + '</div>'
+				);
+		} else {
+			$(".lightbox").append(
+				'<div id="content">' +
+					'<img src="' + img_src + '" id="slides"/>' +
+					'<div id="lightbox_caption">' + caption +
+					'</div>' +
+				'</div>'
+			);
+		}
+
+		$(".lightbox").fadeToggle("fast", function() {
+		});
+	}
+
+	function newSlide() {
+		img_src = $("#" + slide).attr("src");
+		caption = $("#" + slide).attr("alt");
+
+		$("#slides").attr("src", img_src);
+		$("#lightbox_caption").text(caption);
+	}
+
+	function closeLightbox() {
+		$("#content").remove();
+
+		$(".lightbox").fadeToggle("fast", function() {
+		});
+	}
+});
